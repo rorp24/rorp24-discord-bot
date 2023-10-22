33680d4a4ac0fba44ddcd4bcf904c6b9b2c86f9a
rorp <robinelzeard@gmail.com>
2023-10-22 15:49:45 +0200

    première commande fonctionnelle

--- views/accueil_plugin.php
+++ views/accueil_plugin.php
@@ -1,4 +1,8 @@
 <?php
+//A défaut de savoir où mettre le manifest, j'ai décidé de le mettre ici
+//cela évitera que le manifest soit redéclaré trop souvent
+//Dans l'idéal, il faudrai trouver où le caler pour que le manifest ne se redéclare qu'à l'installation et l'update du pluggin (ou genre qu'une fois par jour)
+include __DIR__ .'/../includes/rorp-discord-bot-commands-manifest.php';
 use Discord\BotPrivateData;
 ?>
 
